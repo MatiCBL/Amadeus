36e64643cf5a438f779a888c009eb89325547a7d
MatiCBL <brzoza.mathew@gmail.com>
2023-10-13 22:22:31 +0100

    'added-homepage'

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,33 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import { Flex, Text } from "@chakra-ui/react";
+import { Box, Flex, Text, Button, Link } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <Flex>
-      <Text color="white">Home</Text>
-    </Flex>
+    <Box
+      width="100%"
+      height="100vh"
+      justifyContent="center"
+      alignItems="center"
+      display="flex"
+    >
+      <Flex
+        width="100%"
+        maxW="430px"
+        align="center"
+        justify="center"
+        direction="column"
+        mx="auto"
+      >
+        <Text fontSize="6xl" mb="10px">
+          JournAI
+        </Text>
+        <Text fontSize="1xl">
+          Create travel journal using AI, just type in what you need to plan
+          holidays or business trips.
+        </Text>
+        <Link href="/journal">
+          <Button mt={5}>Get started!</Button>
+        </Link>
+      </Flex>
+    </Box>
   );
 }
